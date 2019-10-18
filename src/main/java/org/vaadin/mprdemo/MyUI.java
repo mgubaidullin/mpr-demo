5139ca9f212562a7ee254b75a8e960a21437dac1
Administrator <marat.gubaidullin@elementai.com>
2019-10-18 11:19:20 -0400

    Vaadin 14

--- src/main/java/org/vaadin/mprdemo/MyUI.java
+++ src/main/java/org/vaadin/mprdemo/MyUI.java
@@ -1,39 +1,34 @@
 package org.vaadin.mprdemo;
 
-import com.vaadin.flow.component.HasElement;
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.applayout.AppLayoutMenu;
-import com.vaadin.flow.component.applayout.AppLayoutMenuItem;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.mpr.core.MprTheme;
+import com.vaadin.ui.UI;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Route("")
 @MprTheme("mytheme")
 public class MyUI extends AppLayout implements RouterLayout {
 
-	private FlexLayout childWrapper = new FlexLayout();
-    private AppLayoutMenu menu = createMenu();
-
     public MyUI () {
         Image img = new Image("https://vaadin.com/images/vaadin-logo.svg", "Vaadin Logo");
         img.setHeight("35px");
-        setBranding(img);
 
-        menu.addMenuItems(
-        		new AppLayoutMenuItem(SpreadsheetView.TITLE, SpreadsheetView.ROUTE),
-        		new AppLayoutMenuItem(TreeView.TITLE, TreeView.ROUTE),
-                new AppLayoutMenuItem(VideoView.TITLE, VideoView.ROUTE));
+        Map<Tab, String> nav = new HashMap();
+        nav.put(new Tab(SpreadsheetView.TITLE), SpreadsheetView.ROUTE);
+        nav.put(new Tab(TreeView.TITLE), TreeView.ROUTE);
+        nav.put(new Tab(VideoView.TITLE), VideoView.ROUTE);
 
-        childWrapper.setSizeFull();
-        setContent(childWrapper);
-                       
+        Tabs tabs = new Tabs(nav.keySet().toArray(new Tab[nav.size()]));
+        tabs.setAutoselect(true);
+        tabs.addSelectedChangeListener(event -> getUI().ifPresent(ui -> ui.navigate(nav.get(event.getSelectedTab()))));
+        addToNavbar(img, tabs);
     }
 
-	@Override
-	public void showRouterLayoutContent(HasElement content) {
-		childWrapper.getElement().appendChild(content.getElement());		
-	}    
 }
